5cf866b6610beb1bae8ab2273d070ca8e210c351
collin foo <collinfoo96@gmail.com>
2023-06-26 15:09:54 +0800

    heatmap map

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -10,11 +10,11 @@ def app():
 
     st.title("Heatmap")
 
-    PLACE_NAME = 'Singapore'
-    G = ox.graph_from_place(PLACE_NAME, network_type='drive')
-    m = ox.plot_graph_folium(G)
+    # PLACE_NAME = 'Singapore'
+    # G = ox.graph_from_place(PLACE_NAME, network_type='drive')
+    # m = ox.plot_graph_folium(G)
 
-    return m
+    # return m
 
     # filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
     # m = leafmap.Map(tiles="stamentoner")
@@ -35,38 +35,38 @@ def app():
 
 # ################# pydeck chart method ####################################3
 
-    # data = {
-    # 'lat': [1.29684825487647, 1.297, 1.298],
-    # 'lon': [103.85253591654006, 103.853, 103.854],
-    # 'time': [12.1, 13.2, 14.3]}
+    data = {
+    'lat': [1.29684825487647, 1.297, 1.298],
+    'lon': [103.85253591654006, 103.853, 103.854],
+    'time': [12.1, 13.2, 14.3]}
 
-    # chart_data = pd.DataFrame(data)
+    chart_data = pd.DataFrame(data)
 
-    # st.pydeck_chart(pdk.Deck(
-    #     map_style=None,
-    #     initial_view_state=pdk.ViewState(
-    #         latitude=1.3521,
-    #         longitude=103.8198,
-    #         zoom=11,
-    #         pitch=30, #camera angle
-    #     ),
-    #     layers=[
-    #         pdk.Layer(
-    #         'HexagonLayer',
-    #         data=chart_data,
-    #         get_position='[lon, lat]',
-    #         radius=50,
-    #         elevation_scale=0,
-    #         elevation_range=[0, 1000],
-    #         pickable=True,
-    #         extruded=True,
-    #         color_range=[
-    #         [0, 0, 255, 100],
-    #         [255, 0, 0, 100]
-    #         ],
-    #         color_domain=[0, 1000],
-    #         tooltip={'text': '@time{%f}'}
-    #         ),
-    #     ],
-    # ))
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=1.3521,
+            longitude=103.8198,
+            zoom=11,
+            pitch=30, #camera angle
+        ),
+        layers=[
+            pdk.Layer(
+            'HexagonLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            radius=50,
+            elevation_scale=0,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+            color_range=[
+            [0, 0, 255, 100],
+            [255, 0, 0, 100]
+            ],
+            color_domain=[0, 1000],
+            tooltip={'text': '@time{%f}'}
+            ),
+        ],
+    ))
 # ################################################################3

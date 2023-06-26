ee9a84f96343f7cf5895ae8f6a7fe4510b9248a5
collin foo <collinfoo96@gmail.com>
2023-06-26 13:46:25 +0800

    heatmap map

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -1,20 +1,23 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import h3
+import folium
 
 
 def app():
 
     st.title("Heatmap")
 
-    filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-    m = leafmap.Map(tiles="stamentoner")
-    m.set_bounds([1.15, 103.55], [1.48, 104.10])
-    m.add_heatmap(
-        filepath,
-        latitude="latitude",
-        longitude="longitude",
-        value="pop_max",
-        name="Heat map",
-        radius=20,
-    )
-    m.to_streamlit(height=700)
+    # filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+    # m = leafmap.Map(tiles="stamentoner")
+    # m.add_heatmap(
+    #     filepath,
+    #     latitude="latitude",
+    #     longitude="longitude",
+    #     value="pop_max",
+    #     name="Heat map",
+    #     radius=20,
+    # )
+    # m.to_streamlit(height=700)
+
+    map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")

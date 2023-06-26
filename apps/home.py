8943f9167566c7e407307c1449e02d9ebd0491ad
collin foo <collinfoo96@gmail.com>
2023-06-26 15:41:11 +0800

    requirements

--- apps/home.py
+++ apps/home.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import leafmap
-# import leafmap.foliummap as leafmap
+import leafmap.foliummap as leafmap
 
 def app():
     st.title("Connect AI")
@@ -12,7 +11,7 @@ def app():
     """
     )
 
-    # m = leafmap.Map(locate_control=True)
-    # m = leafmap.Map(center=(1.3521, 103.8198), zoom=12)
-    # m.add_basemap("ROADMAP")
-    # m.to_streamlit(height=700)
+    m = leafmap.Map(locate_control=True)
+    m = leafmap.Map(center=(1.3521, 103.8198), zoom=12)
+    m.add_basemap("ROADMAP")
+    m.to_streamlit(height=700)

4ca4c99482139c43ab0650591299455d6e8ad9ee
collin foo <collinfoo96@gmail.com>
2023-06-26 12:29:09 +0800

    set singapore bounds

--- apps/home.py
+++ apps/home.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-import osmnx as ox
-import folium
-from streamlit_folium import st_folium
 
 
 def app():
@@ -10,17 +7,12 @@ def app():
 
     st.markdown(
         """
-    Welcome to Connect AI! Where we connect you with your dates iykwim
+    Welcome to Connect AI! Where we connect you with your dates
 
     """
     )
 
-    # m = leafmap.Map(locate_control=True)
-    # m = leafmap.Map(center=(1.3521, 103.8198), zoom=12)
-    # m.add_basemap("ROADMAP")
-    # m.to_streamlit(height=700)
-
-    PLACE_NAME = 'Singapore'
-    G = ox.graph_from_place(PLACE_NAME, network_type='drive')
-    m = ox.plot_graph_folium(G)
-    st_data = st_folium(m, width=700)
+    m = leafmap.Map(locate_control=True)
+    m = leafmap.Map(center=(1.3521, 103.8198), zoom=12)
+    m.add_basemap("ROADMAP")
+    m.to_streamlit(height=700)

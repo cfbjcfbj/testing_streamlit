5132a477da171aa5a94218dfb9950ae9bcda4fb1
collin foo <collinfoo96@gmail.com>
2023-06-26 14:10:36 +0800

    heatmap map

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -46,12 +46,5 @@ def app():
             pickable=True,
             extruded=True,
             ),
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=chart_data,
-                get_position='[lon, lat]',
-                get_color='[200, 30, 0, 160]',
-                get_radius=200,
-            ),
         ],
     ))

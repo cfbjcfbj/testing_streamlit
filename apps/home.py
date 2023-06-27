a47da7b224e291f309eec88be76063e996218c15
collin foo <collinfoo96@gmail.com>
2023-06-27 12:59:56 +0800

    requirements

--- apps/home.py
+++ apps/home.py
@@ -5,24 +5,23 @@ import h3
 import branca.colormap as cm
 import requests
 
-st.title("Connect AI")
-
-st.markdown(
-        """
-        Welcome to Connect AI! Where we connect you with your dates
-        """
-)
-
-# Create a text input box
-user_input = st.text_input("Where are you now?", "")
-
-# Display the input text
-st.write("You are here at~", user_input)
-
-
 
 # @st.cache(allow_output_mutation=True)
 def app():
+
+    st.title("Connect AI")
+
+    st.markdown(
+        """
+        Welcome to Connect AI! Where we connect you with your dates
+        """
+        )
+
+    # Create a text input box
+    user_input = st.text_input("Where are you now?", "")
+
+    # Display the input text
+    st.write("You are here at~", user_input)
 
     # Call API
 

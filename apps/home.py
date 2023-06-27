755d6e6a3e87accd9b4ce38ad5e1f1305269c8c7
collin foo <collinfoo96@gmail.com>
2023-06-27 13:15:23 +0800

    requirements

--- apps/home.py
+++ apps/home.py
@@ -5,9 +5,7 @@ import h3
 import branca.colormap as cm
 import requests
 
-
-# @st.cache(allow_output_mutation=True)
-def app():
+def text():
 
     st.title("Connect AI")
 
@@ -23,50 +21,49 @@ def app():
     # Display the input text
     st.write("You are here at~", user_input)
 
-    # Check if user input has changed
-    if user_input != new_input:
-        new_input = user_input
+# @st.cache(allow_output_mutation=True)
+def app():
 
-        # Call API
+    # Call API
 
-        response = requests.get(f"https://connectai-emwgdoqmma-de.a.run.app/traveltimeh3?locations={new_input}")
-        if response.status_code == 200:
-            hex_data = response.json()
-        else:
-            st.write("Error:", response.status_code)
+    response = requests.get(f"https://connectai-emwgdoqmma-de.a.run.app/traveltimeh3?locations={user_input}")
+    if response.status_code == 200:
+        hex_data = response.json()
+    else:
+        st.write("Error:", response.status_code)
 
-        hexagons = {}
+    hexagons = {}
 
-        for key, value in hex_data.items():
-            hexagon_id = list(value.keys())[0]
-            measurement = list(value.values())[0]
-            hexagons[hexagon_id] = measurement
+    for key, value in hex_data.items():
+        hexagon_id = list(value.keys())[0]
+        measurement = list(value.values())[0]
+        hexagons[hexagon_id] = measurement
 
-        # Plot map
-        map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")
+    # Plot map
+    map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")
 
-        # Add click event handler
-        map.add_child(folium.LatLngPopup())
-
-
-        color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
+    # Add click event handler
+    map.add_child(folium.LatLngPopup())
 
 
-        for h3_index, time in hexagons.items():
-            geo_boundary = h3.h3_to_geo_boundary(h3_index, geo_json=False)
+    color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
 
-            color = color_scale(time)
 
-            folium.Polygon(
-                locations=geo_boundary,
-            color=None,
-            fill=True,
-            fill_color=color,
-            fill_opacity=0.6,
-            popup=f'Time: {time:.2f} min'
-            ).add_to(map)
+    for h3_index, time in hexagons.items():
+        geo_boundary = h3.h3_to_geo_boundary(h3_index, geo_json=False)
 
-        st_folium(map, width=1400, height=700)
+        color = color_scale(time)
+
+        folium.Polygon(
+            locations=geo_boundary,
+        color=None,
+        fill=True,
+        fill_color=color,
+        fill_opacity=0.6,
+        popup=f'Time: {time:.2f} min'
+        ).add_to(map)
+
+    st_folium(map, width=1400, height=700)
 
 
 if __name__ == "__main__":

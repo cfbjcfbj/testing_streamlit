3ddd2bb2e286ed9ecf3d7954eb4d82a9947198d7
collin foo <collinfoo96@gmail.com>
2023-06-27 16:21:14 +0800

    requirements

--- apps/home.py
+++ apps/home.py
@@ -1,30 +1,12 @@
 import streamlit as st
 import folium
-from streamlit_folium import st_folium
+from streamlit_folium import folium_static
 import h3
 import branca.colormap as cm
 import requests
 
 
-@st.cache_data()
-def app():
-
-    st.title("Connect AI")
-
-    st.markdown(
-        """
-        Welcome to Connect AI! Where we connect you with your dates
-        """
-        )
-
-    # Create a text input box
-    user_input = st.text_input("Where are you now?", "")
-
-    # Display the input text
-    st.write("You are here at~", user_input)
-
-    # Call API
-
+def app(user_input):
     response = requests.get(f"https://connectai-emwgdoqmma-de.a.run.app/traveltimeh3?locations={user_input}")
     if response.status_code == 200:
         hex_data = response.json()
@@ -44,9 +26,7 @@ def app():
     # Add click event handler
     map.add_child(folium.LatLngPopup())
 
-
     color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
-
 
     for h3_index, time in hexagons.items():
         geo_boundary = h3.h3_to_geo_boundary(h3_index, geo_json=False)
@@ -55,15 +35,30 @@ def app():
 
         folium.Polygon(
             locations=geo_boundary,
-        color=None,
-        fill=True,
-        fill_color=color,
-        fill_opacity=0.6,
-        popup=f'Time: {time:.2f} min'
+            color=None,
+            fill=True,
+            fill_color=color,
+            fill_opacity=0.6,
+            popup=f'Time: {time:.2f} min'
         ).add_to(map)
 
-    st_folium(map, width=1500, height=700)
+    folium_static(map)
+
+
+def main():
+    st.title("Connect AI")
+    st.markdown("Welcome to Connect AI! Where we connect you with your dates")
+
+    # Create a text input box
+    user_input = st.text_input("Where are you now?", "")
+
+    # Display the input text
+    st.write("You are here at~", user_input)
+
+    # Call app when Enter is pressed
+    if st.button("Submit"):
+        app(user_input)
 
 
 if __name__ == "__main__":
-    app()
+    main()
